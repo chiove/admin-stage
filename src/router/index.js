d6d32f682e9904b936f17dada1c671a8ac44a849
chiove <240718742@qq.com>
2018-08-11 12:06:33 +0800

    初始化

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/login', component: resolve => require(['@/components/contain/login'],resolve)
+    },
+    {
+      path: '/index',
+      redirect:'/index/dailyData',
+      component: resolve => require(['@/components/contain/index'],resolve),
+      children:[
+      {
+        path: '/index/dailyData',
+        component: resolve => require(['@/components/containFrame/dailyData'],resolve),
+      },
+      {
+        path: '/index/weekData',
+        component: resolve => require(['@/components/containFrame/weekData'],resolve),
+      },
+      {
+        path: '/index/studentsCare',
+        component: resolve => require(['@/components/containFrame/studentsCare'],resolve),
+      },
+      {
+        path: '/index/studentsSearch',
+        component: resolve => require(['@/components/containFrame/studentsSearch'],resolve),
+      },
+      {
+        path: '/index/teacherCheck',
+        component: resolve => require(['@/components/containFrame/teacherCheck'],resolve),
+      },
+      {
+        path: '/index/studentsDetails',
+        component: resolve => require(['@/components/containFrame/studentsDetails'],resolve),
+      }
+      ]
+    },
+  ]
+})
+
